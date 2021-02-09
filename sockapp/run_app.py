92cb01d37261bda3506c088cee48ac6f09955259
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2021-02-09 23:52:33 +0530

    Add default browser opening functionality, resolves #6

--- sockapp/run_app.py
+++ sockapp/run_app.py
@@ -1,4 +1,7 @@
+import webbrowser, threading
+
 from .app import app
 
 def run_app():
-    app.run()
+    threading.Timer(0.5, lambda: webbrowser.open("http://localhost:5000")).start()
+    app.run(debug=False)
