4adae6373f03a99abce23f4b817c5042b4f37b36
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2021-02-10 21:06:04 +0530

    Add port as command line argument, resolves #9

--- sockapp/run_app.py
+++ sockapp/run_app.py
@@ -1,8 +1,15 @@
-import webbrowser, threading
+import argparse
+import threading
+import webbrowser
 
 from .app import app
 
 def run_app():
     threading.Timer(0.5, lambda: webbrowser.open("http://localhost:5000")).start()
-    app.config['port'] = 1234
+
+    parser = argparse.ArgumentParser(description="SockApp - Socket based file sharing app")
+    parser.add_argument("--port", default=5001, help="Port in which socket will run")
+    args = parser.parse_args()
+
+    app.config['port'] = args.port
     app.run(debug=False)
